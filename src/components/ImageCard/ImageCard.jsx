e10684ee55d4ca89693eff3420cd66fe9c9dd02a
dmeshkovGit <140300183+dmeshkovGit@users.noreply.github.com>
2024-03-05 01:36:07 +0200

    fix

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -3,7 +3,7 @@ import css from './ImageCard.module.css'
 export default function ImageCard({ image, onClick }) {
     return (
 		<div className={css.imageWrapper}>
-			<img className={css.image} onClick={() => { onClick(image.urls.regular, image.alt_description) }} src={image.urls.regular} alt={image.alt_description} />
+			<img className={css.image} onClick={() => { onClick(image.urls.regular, image.alt_description) }} src={image.urls.small} alt={image.alt_description} />
 		</div>
     )
 }
